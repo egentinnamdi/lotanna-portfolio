d5b0f75865875834c065b52d1aa62dcc45d6da27
egentinnamdi <egentinnamdi10@gmail.com>
2026-07-20 10:21:28 +0100

    style(testimonials): add top margin to testimonials section

--- components/testimonials/TestimonialsSection.tsx
+++ components/testimonials/TestimonialsSection.tsx
@@ -3,7 +3,7 @@ import TestimonialsGridView from "./TestimonialsGridView"
 
 export default function TestimonialsSection() {
   return (
-    <section id="testimonials" className="flex flex-col gap-10 lg:gap-10">
+    <section id="testimonials" className="mt-20 flex flex-col gap-10 lg:gap-10">
       <h2 className="text-2xl leading-normal font-medium lg:w-2/4 lg:text-3xl">
         Backed by real feedback from the people I've worked with most.
       </h2>
